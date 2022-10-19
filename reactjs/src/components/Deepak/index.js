4addfcd9c44630b483c1fd06135180a8428791d1
Deepak <ch.deepak231@gmail.com>
2022-10-19 21:17:07 +0530

    PC-1

--- reactjs/src/components/Deepak/index.js
+++ reactjs/src/components/Deepak/index.js
@@ -6,7 +6,26 @@ export default class Deepak extends Component {
   render() {
     return (
       <div>
-        <h1>Deepak</h1>
+          <div class="bg-container">
+          <div class="card-container">
+              <h1 class="main-heading">Start Musical Night</h1>
+              <div class="card-body">
+                  <img src="https://assets.ccbp.in/frontend/intermediate-rwd/musical-night-1-img.png" class="image" />
+                  <h1 class="musical-night">
+                      We bring you online experiences to make sure you've got an exciting plan every day.
+                  </h1>
+                  <p class="description">
+                      We still will have fun and we are much more confident on this as we alredy had seen some happy faces in our previous multiple online events!
+                  </p>
+                  <h2 class="musical-night">About the Event</h2>
+                  <p class="description">
+                      We, humans, are social creatures. While many of us are making the best of this time, we're much better together than apart.
+                  </p>
+                  <img src="https://assets.ccbp.in/frontend/intermediate-rwd/musical-night-2-img.png" class="muscial-night-singer" />
+                  <img src="https://assets.ccbp.in/frontend/intermediate-rwd/musical-night-3-img.png" class="muscial-night-singer" />
+              </div>
+          </div>
+        </div>
       </div>
     )
   }
