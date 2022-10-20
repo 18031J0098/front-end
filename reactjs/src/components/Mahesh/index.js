4b8501e91aed09b9063b33e8275c7b611f42cf80
maheshkada1217 <mailmemahesh08@gmail.com>
2022-10-20 14:27:56 +0530

    Musical web page added to mahesh component

--- reactjs/src/components/Mahesh/index.js
+++ reactjs/src/components/Mahesh/index.js
@@ -5,8 +5,22 @@ import React, { Component } from 'react'
 export default class Mahesh extends Component {
   render() {
     return (
-      <div>
-        <h1>Mahesh</h1>
+      <div className='bg-container'>
+        <div className='card-container'>
+          <h1>Start Musical Night</h1>
+          <hr />
+          <div className='inner-card'>
+            <img src="https://assets.ccbp.in/frontend/intermediate-rwd/musical-night-1-img.png" alt="logo" className='image' />
+            <h3>We bring you online experiences to make sure you've got an exciting plan every day.</h3>
+            <p>We still have fun and we are much more confident on this as we already had senn some happy faces in our previous multiple online events!</p>
+            <h3>About the Event</h3>
+            <p>We, humans, are social creatures. While many of us are making thebest of this time, we're much better together than apart.</p>
+            <div>
+              <img src="https://assets.ccbp.in/frontend/intermediate-rwd/musical-night-2-img.png" alt="logo" className='image1' />
+              <img src="https://assets.ccbp.in/frontend/intermediate-rwd/musical-night-3-img.png" alt="logo" className='image1' />
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
