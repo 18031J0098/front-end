3d940826d416bd1659523e5a17dd97ce97e21519
Sandeep <Sandeepyadavkatla19977@gmail.com>
2022-10-27 21:19:47 +0530

    sandeep new code

--- reactjs/src/components/Sandeep/index.js
+++ reactjs/src/components/Sandeep/index.js
@@ -3,7 +3,21 @@ import React, { Component } from 'react'
 export default class Sandeep extends Component {
   render() {
     return (
-      <div>Sandeep yadav</div>
+      <div className='main'>
+      <div> 
+          <div className="container">
+            <h1 className="heading">THE MUSIC WORLD</h1>
+            <h4 className="description">Music is your own experience,your thoughts, your wisdom.If you don"t live it,
+            <br></br>you are not listening to the right kind</h4>
+            <div className="button-container">
+              <button class="button1">Get started</button>
+              <button class="button2">Learn More</button>
+            </div>
+          </div>
+          
+      </div>
+      </div>
+
     )
   }
 }
