0aa78f6b31d32768ee5587c1c935eee3b4fe8683
Deepak <ch.deepak231@gmail.com>
2022-10-27 21:22:15 +0530

    deepak updated code

--- reactjs/src/components/Deepak/index.js
+++ reactjs/src/components/Deepak/index.js
@@ -1,12 +1,11 @@
-import './index.css'
-
 import React, { Component } from 'react'
+import './index.css'
 
 export default class Deepak extends Component {
   render() {
     return (
       <div>
-          <div class="bg-container">
+          {/* <div class="bg-container">
           <div class="card-container">
               <h1 class="main-heading">Start Musical Night</h1>
               <div class="card-body">
@@ -25,7 +24,36 @@ export default class Deepak extends Component {
                   <img src="https://assets.ccbp.in/frontend/intermediate-rwd/musical-night-3-img.png" class="muscial-night-singer" />
               </div>
           </div>
+        </div> */}
+        {/* <div class="bg-container">
+        <h1 class="main-heading">Nature Gallery</h1>
+        <div class="card-container">
+            <div class="images-container">
+                <img src="https://assets.ccbp.in/frontend/intermediate-rwd/nature-gallery-1-img.png" class="image" />
+                <img src="https://assets.ccbp.in/frontend/intermediate-rwd/nature-gallery-2-img.png" class="image" />
+                <img src="https://assets.ccbp.in/frontend/intermediate-rwd/nature-gallery-3-img.png" class="image" />
+            </div>
         </div>
+      </div> */}
+
+      {/* <div class="boxes-container">
+          <div className='box box1'>1</div>
+          <div className='box box2'>2</div>
+          <div className='box box3'>3</div>
+      </div> */}
+        
+       <div className='container'>
+        <p className='heading'> THE MUSIC WORLD</p>
+        <p className='description'>
+            Music is your own experience, your thoughts, your wisdom. If you don’t
+            live it, you're not listening to the right kind.
+        </p>
+        <div className='button-container'>
+            <button className='button'>Get Started</button>
+            <button className='outline-button'>Learn More</button>
+        </div>
+       </div>
+
       </div>
     )
   }
