6e90b186c5a00955b3eae80aab91bb63232be492
Sandeep <Sandeepyadavkatla19977@gmail.com>
2022-10-27 21:35:45 +0530

    sandeep modified code

--- reactjs/src/components/Sandeep/index.js
+++ reactjs/src/components/Sandeep/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import './index.css';
 
 export default class Sandeep extends Component {
   render() {
     return (
       <div className='main'>
       <div> 
-          <div className="container">
+          <div className="container-bg">
             <h1 className="heading">THE MUSIC WORLD</h1>
             <h4 className="description">Music is your own experience,your thoughts, your wisdom.If you don"t live it,
             <br></br>you are not listening to the right kind</h4>
