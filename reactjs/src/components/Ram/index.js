91e096891bbd5efda855011b3e24c81aef23a95a
rambabu567 <rams.gandam@gmail.com>
2022-10-28 16:57:14 +0530

    ram code commit

--- reactjs/src/components/Ram/index.js
+++ reactjs/src/components/Ram/index.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
-
+import './index.css';
 export default class Ramu extends Component {
   render() {
     return (
-      <div>Ramu</div>
+      <div className='main'>
+       <div>
+          <div className="container">
+          <div className="container-ram">
+            <h1 className="heading">THE MUSIC WORLD</h1>
+            <h4 className="description">Music is your own experience,your thoughts, your wisdom.If you don"t live it,
+            <br></br>you are not listening to the right kind</h4>
+            <div className="button-container">
+              <button class="button1">Get started</button>
+              <button class="button2">Learn More</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     )
   }
 }
